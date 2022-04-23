d62b9b9e502f0e7dbffca4a97bd3c0cc95a8e46a
Ngoni Munyaradzi <ngoni.munyaradzi@gmail.com>
2022-04-23 06:40:03 +0200

    Additional project setup

--- app/src/main/kotlin/com/scribblex/creditscore/ui/MainActivity.kt
+++ app/src/main/kotlin/com/scribblex/creditscore/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.scribblex.creditscore
+package com.scribblex.creditscore.ui
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +9,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.scribblex.creditscore.R
 import com.scribblex.creditscore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
